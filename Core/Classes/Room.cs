3c81136f092ea0a570081f304c85da4ba7005d30
Galina <g-tsoi.n372@mail.ru>
2018-06-03 14:10:33 +0300

    first commit

---
+++ Core/Classes/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Classes
+{
+    class Room
+    {
+        public int RoomNumber { get; set; }
+        public int Key { get; set; }
+    }
+}
