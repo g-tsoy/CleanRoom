d1b17f774dc9d8cb53346d883f5804c07a76f348
nguyenql <endless1164@yandex.ru>
2018-06-05 20:02:29 +0300

    Add project files.

---
+++ Core/Classes/Reminder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Classes
+{
+    class Reminder
+    {
+        public string Text { get; set; }
+        public User From { get; set; }
+        public DateTime DateOfSending { get; set; }
+    }
+}
---
+++ RoomCleaning/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoomCleaning
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickEnterYourRoom(object sender, RoutedEventArgs e)
+        {
+            Schedule window = new Schedule();
+            window.ShowDialog();
+        }
+
+        private void ButtonClickRegistrateUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClickRegistrateRoom(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
